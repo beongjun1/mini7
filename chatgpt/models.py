cf8ea7e2edfa11fb03198120d962b6643de9b472
sunjin <sjp0402@gmail.com>
2024-06-12 14:26:17 +0900

    user 구분 기능 추가

--- chatgpt/models.py
+++ chatgpt/models.py
@@ -12,6 +12,7 @@ class Chat(models.Model):
     # 썸네일 - 새로운 방id 생길때 최초 유저 메세지 앞 10글자
     thumbnail = models.CharField(max_length=10)
     timestamp = models.DateTimeField(auto_now_add=True)
+    user_id = models.CharField(max_length=255, default='admin')
     
     def __str__(self):
         return self.thumbnail
@@ -21,7 +22,7 @@ class ChatRoom(models.Model):
     
 class Message(models.Model):
     chat = models.ForeignKey(Chat, on_delete=models.CASCADE)
-    user_id = models.CharField(max_length=255, default='1')
+    user_id = models.CharField(max_length=255, default='admin')
     user = models.CharField(max_length=255)
     text = models.TextField()
     timestamp = models.DateTimeField(auto_now_add=True)
--- chatgpt/urls.py
+++ chatgpt/urls.py
@@ -5,7 +5,8 @@ from . import views
 app_name = 'chatgpt'
 urlpatterns = [
     path('', views.chat_view, name='chat_view'),
-    path('chat_view/<int:chat_id>/', views.chat_view, name='chat_view'),
+    path('chat_view/<str:user_id>/', views.chat_view, name='chat_view_user'),
+    path('chat_view/<str:user_id>/<int:chat_id>/', views.chat_view, name='chat_view'),
     path('api/chat/', views.chat, name='chat'),
     path('add_room/', views.add_chat_room, name='chatgpt:add_chat_room'),
     path('download/', views.download, name='download_chat_history'),
