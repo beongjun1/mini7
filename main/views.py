170c57e006f49e7d56b71ab1cbbf45610cb8c5b3
byeongjun <beongjun1@gmail.com>
2024-06-13 10:43:17 +0900

    css 적용

--- main/views.py
+++ main/views.py
@@ -4,16 +4,20 @@ from django.conf import settings
 from django.contrib.auth.views import PasswordChangeView
 from django.urls import reverse_lazy
 from django.contrib import messages
-
-    
-
-
         
 
 def main_view(request):
     return render(request, 'main.html')
 
 
+def greet(request):
+    return render(request, 'greet.html')
+
+def qa(request):
+    return render(request, 'qa.html')
+
+def program(request):
+    return render(request, 'program.html')
 
     
 
